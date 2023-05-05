78404785e0ca719145aae7f5f523ef4ba518d00f
RyukShi <aure16300P@gmail.com>
2023-05-05 10:49:16 +0200

    counting-sort.c source file created

--- src/sorting-algorithms.c
+++ src/sorting-algorithms.c
@@ -1,35 +1,6 @@
 #include "./sorting-algorithms.h"
 #include <stdlib.h>
 
-void	counting_sort(Array *array)
-{
-	int	range;
-	int	*count;
-	int	*output;
-
-	range = array->max - array->min + 1;
-	count = (int *)calloc(range, sizeof(int));
-	output = (int *)malloc(array->size * sizeof(int));
-	if (!count || !output)
-	{
-		fprintf(stderr, "Error: memory allocation failed.\n");
-		exit(1);
-	}
-	for (unsigned int i = 0; i < array->size; i++)
-		count[array->data[i] - array->min]++;
-	for (int i = 1; i < range; i++)
-		count[i] += count[i - 1];
-	for (int i = array->size - 1; i >= 0; i--)
-	{
-		output[count[array->data[i] - array->min] - 1] = array->data[i];
-		count[array->data[i] - array->min]--;
-	}
-	for (unsigned int i = 0; i < array->size; i++)
-		array->data[i] = output[i];
-	free(count);
-	free(output);
-}
-
 static void	counting_sort_two(Array *array, int place)
 {
 	int	freq[10] = {0};
