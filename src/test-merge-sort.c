b43cd729618638530844cb546cd9e7b9a161b8d7
RyukShi <aure16300P@gmail.com>
2023-05-11 19:24:26 +0200

    unit test merge sort done

---
+++ src/test-merge-sort.c
@@ -0,0 +1,14 @@
+#include "./merge-sort.h"
+#include <assert.h>
+#include <stdbool.h>
+
+int	main(void)
+{
+	Array	*random_arr;
+
+	random_arr = random_array(1000);
+	find_min_and_max(random_arr);
+	merge_sort(random_arr, 0, random_arr->size - 1);
+	assert(is_sorted(random_arr) == true);
+	destroy_array(random_arr);
+}
